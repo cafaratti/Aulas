eb7a30bc5d3c9f47645ea90ec1c53acfb25f14b6
cafaratti <bruna.04689@aluno.iffar.edu.br>
2026-04-13 08:37:39 -0300

    jk

--- b/exer7.php
+++ b/exer7.php
@@ -4,12 +4,10 @@ $val1= $_GET ["val1"];
 $val2= $_GET ["val2"];
 $vet1= explode (" ", $val1);
 $vet2= explode (" ", $val2);
-$o=3;
 for ($i=0; $i<4; $i++)
 {
-	$vet3[$i] = $vet1 [$i] + $vet2 [$o];
-    echo "$vet3 [$i] <br>";
-    $o= $o-1;
+	$vet3[$i] = $vet1 [$i] + $vet2 [3-$i];
+    print_r ("$vet3[$i] <br>");
 }
 
 ?>
