bf3e80d3cdb85695bc90f8104afbd5fd458c0bb4
cafaratti <bruna.04689@aluno.iffar.edu.br>
2026-05-11 15:09:19 -0300

    ghmh

---
+++ b/exer5inicio.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="exer5processamento.php" method="get">
+    <h1> Cadastro </h1>
+    Seu nome: <input type="text" name="pessoa['nome']">
+    Sexo <input type="text" name="pessoa['sexo']">
+    Telefone <input type="text" name="pessoa['telefone']">
+    Data de nascimento <input type="text" name="pessoa['data']">
+    <input type="submit" value="enviar">
+</form>
+</body>
+</html>
---
+++ b/leitura.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="processamento.php" method="get">
+         <h1> Digite suas notas </h1>
+   <?php
+        $quant= $_GET ["quant"];
+        for ($i=0; $i<$quant; $i++)
+            {
+                echo "<input type='text' name='notas[]' placeholder='digite a nota o aluno $i'>";
+            }
+        ?>
+    <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
