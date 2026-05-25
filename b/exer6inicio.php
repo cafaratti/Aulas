91bce58ceba86042ac4a30da48e18dabe8a98da1
cafaratti <bruna.04689@aluno.iffar.edu.br>
2026-05-25 14:44:36 -0300

    abu

---
+++ b/exer6inicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="exer6leitura.php" method="get">
+    <LEGEnd>AI AAI AAAII</LEGEnd>
+        Escreva o nome de um país: <input type="text" name="pesquisa">
+
+        <input type="submit" value="enviar">
+    </form>
+</body>
+</html>
