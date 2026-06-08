dba5e990ae27c56ceac81960a4ad8fa0c7e47e3e
cafaratti <bruna.04689@aluno.iffar.edu.br>
2026-06-08 15:06:17 -0300

    56

---
+++ b/exer4inicio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="exer4leitura.php" method="get">   
+    Altura do salto do sapo: <input type="text" name="salto">
+    Número de canos: <input type="text" name="canos">
+    <input type="submit" value="enviar">
+</form>
+</body>
+</html>
---
+++ b/vestinicio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="vestleitura.php" method="get">
+    Número de questões: <input type="text" name="questoes">
+    Gabarito: <input type="text" name="gabarito">
+    Gabarito candidato: <input type="text" name="candidato">
+    <input type="submit" value="enviar">
+</form>
+</body>
+</html>
