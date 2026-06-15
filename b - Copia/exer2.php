6a40b92a557c822e2ff07b86f146ae243d55f32d
cafaratti <bruna.04689@aluno.iffar.edu.br>
2026-06-15 09:11:37 -0300

    Delete b - Copia directory

--- b - Copia/exer2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$vet= range (3,60,3);
-for ($i=0; $i<20; $i++)
-    {
-        echo "$vet[$i] <br>";
-    }
-?>
--- b - Copia/exer3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$val= $_GET ["valores"];
-$vet= explode (" ", $val);
-for ($i=9; $i>=0; $i= $i-1)
-    {
-        echo "$vet[$i] <br>";
-    }
-?>
